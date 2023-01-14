ec5c2f4630c68833ca580604b662590f3a6568e7
Suel_ki <116934864+Suel-ki@users.noreply.github.com>
2023-01-14 20:43:12 +0800

    Update RenderType for Picture Frame

--- src/main/java/com/github/suel_ki/beautify/BeautifyClient.java
+++ src/main/java/com/github/suel_ki/beautify/BeautifyClient.java
@@ -13,7 +13,7 @@ public class BeautifyClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        // particle
+        // PARTICLE
         ParticleInit.registerParticle();
 
         // BLOCKS
@@ -21,15 +21,15 @@ public class BeautifyClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.HANGING_POT, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.ROPE, RenderType.cutout());
 
-        // lamps
+        // LAMPS
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.LAMP_JAR, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.LAMP_BAMBOO, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.LAMP_LIGHT_BULB, RenderType.cutout());
 
-        // workbench
+        // WORKBENCH
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.BOTANIST_WORKBENCH, RenderType.cutout());
 
-        // trellis
+        // TRELLIS
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.OAK_TRELLIS, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.SPRUCE_TRELLIS, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.BIRCH_TRELLIS, RenderType.cutout());
@@ -40,7 +40,7 @@ public class BeautifyClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.CRIMSON_TRELLIS, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.WARPED_TRELLIS, RenderType.cutout());
 
-        // blinds
+        // BLINDS
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.SPRUCE_BLINDS, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.DARK_OAK_BLINDS, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.CRIMSON_BLINDS, RenderType.cutout());
@@ -51,5 +51,17 @@ public class BeautifyClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.WARPED_BLINDS, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.MANGROVE_BLINDS, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.IRON_BLINDS, RenderType.cutout());
+
+        // FRAMES
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.SPRUCE_PICTURE_FRAME, RenderType.cutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.DARK_OAK_PICTURE_FRAME, RenderType.cutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.CRIMSON_PICTURE_FRAME, RenderType.cutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.ACACIA_PICTURE_FRAME, RenderType.cutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.JUNGLE_PICTURE_FRAME, RenderType.cutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.OAK_PICTURE_FRAME, RenderType.cutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.BIRCH_PICTURE_FRAME, RenderType.cutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.WARPED_PICTURE_FRAME, RenderType.cutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.MANGROVE_PICTURE_FRAME, RenderType.cutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.QUARTZ_PICTURE_FRAME, RenderType.cutout());
     }
 }
